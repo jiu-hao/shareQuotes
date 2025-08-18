18df64955418ce126b4bc15a314d645f9df372c5
linyanshan <1294254846@qq.com>
2025-08-18 17:14:45 +0800

    chore:【Vuepres】部署项目

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/jiu-hao/shareQuotes.git
+git push -f https://jiu-hao.github.io/shareQuotes
 
 cd -
 
